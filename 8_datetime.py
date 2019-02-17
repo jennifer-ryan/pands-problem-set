4b7d4b88b7ce26f0daeac77258649e7e8fa87e41
Jennifer Ryan <G00376325@gmit.ie>
2019-02-17 16:45:24 +0000

    Problem 8. Day and month. Is it too complex?

--- 8_datetime.py
+++ 8_datetime.py
@@ -3,27 +3,58 @@
 
 # testing out datetime module
 # datetime.now() prints year-month-day-time
-# should I create functions for each part?
-# function day created to get the day, comma and space
-
+# should I create functions for each part? tried on first commit
+# maybe I don't need functions
+# day and month done. Is there an easier way though??
 
 from datetime import datetime
 
-def day():
-    today = datetime.today().weekday()
-    if today == 0:
-        print("Monday, ")
-    elif today == 1:
-        print("Tuesday, ")
-    elif today == 2:
-        print("Wednesday, ")
-    elif today == 3:
-        print("Thursday, ")
-    elif today == 4:
-        print("Friday, ")
-    elif today == 5:
-        print("Saturday, ")
-    elif today == 6:
-        print("Sunday, ")                    
-    
-day()   
+day = datetime.today().weekday()
+d = ""
+if day == 0:
+    d = "Monday,"
+elif day == 1:
+    d = "Tuesday,"
+elif day == 2:
+    d = "Wednesday,"
+elif day == 3:
+    d = "Thursday,"
+elif day == 4:
+    d = "Friday,"
+elif day == 5:
+    d = "Saturday,"
+elif day == 6:
+    d = "Sunday,"                    
+
+
+month = datetime.today().month  
+m = ""
+if month == 1:
+    m = "January"
+elif month == 2:
+    m = "February"
+elif month == 3:
+    m = "March"    
+elif month == 4:
+    m = "April"
+elif month == 5:
+    m = "May"
+elif month == 6:
+    m = "June"
+elif month == 7:
+    m = "July"
+elif month == 8:
+    m = "August"
+elif month == 9:
+    m = "September"
+elif month == 10:
+    m = "October"
+elif month == 11:
+    m = "November"
+elif month == 12:
+    m = "December"
+
+
+
+
+print(d, m)
