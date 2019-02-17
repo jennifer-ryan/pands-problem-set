3f1e9b8608552aaecd2258b1de8d6ec172896877
Jennifer Ryan <G00376325@gmit.ie>
2019-02-17 15:23:35 +0000

    Problem 6 added - not attempted yet

---
+++ 6_second_string.py
@@ -0,0 +1,5 @@
+# Write a program that takes a user input string and outputs every second word.
+
+# Possible to index each word by dividing the sentence according to the space between them?
+
+sentence = input("Please enter a sentence: ")
