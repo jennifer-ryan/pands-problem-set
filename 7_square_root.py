1a886f35cff7134d0ea6594fde5084b3a82b0bd9
Jennifer Ryan <G00376325@gmit.ie>
2019-02-23 14:36:37 +0000

    Accounted for integers with exact square roots

--- 7_square_root.py
+++ 7_square_root.py
@@ -6,11 +6,19 @@
 # add float before input
 # format s so there is only 1 decimal place like in example? round()
 # how to add full-stop to end like in example? Change s to string format and concatenate.
-# should I account for integers with exact square roots as these answers won't be approximate? 
+
+# should I account for integers with whole number square roots as these answers won't be approximate? 
+# to account for this I get help here https://stackoverflow.com/q/4541155
+# if s is the same as int(s), the number is a whole number square root and not approximate so the output is adjusted.
 
 from math import sqrt
 
 i = float(input("Please enter a number: "))
 
 s = round(sqrt(i), 1)
-print("The square root of", i, "is approximately " + str(s) + ".")
+
+if s != int(s):
+    print("The square root of", i, "is approximately " + str(s) + ".")
+else:
+    print("The square root of", int(i), "is " + str(int(s)) + ".")    
+
