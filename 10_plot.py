4324f23fe528a00cb8f43799122bea545b723715
Jennifer Ryan <G00376325@gmit.ie>
2019-03-05 21:50:47 +0000

    Problem 10 - may be finished if I'm understanding the question correctly

--- 10_plot.py
+++ 10_plot.py
@@ -1,27 +1,38 @@
 # Write a program that displays a plot of the functions x, x**2 and 2**x in the range [0, 4].
 
-# My maths aren't great...
-# Assuming x will be the numbers within the range?
-# Trying matplotlib tutorial https://matplotlib.org/users/pyplot_tutorial.html
+# Matplotlib tutorial https://matplotlib.org/users/pyplot_tutorial.html
+# plt.plot(x values, y values)
+# x values are range[0, 4] - does this want me to include no. 4? range is within square (list) brackets rather than parentheses so I will include no. 4.  
+# # y = f(x) https://www.youtube.com/watch?v=2-dUHLHeyTY so y values will be each function (x, x**2 and 2**x) 
 
+# help from this example: https://stackoverflow.com/a/46726953 
 
-
-# example from https://stackoverflow.com/a/46726953 adjusted below.
-# is this what the problem is looking for?
-# Outputs 3 different lines of the graph
+# import matplotlib in order to produce a graph.
 import matplotlib.pyplot as plt
 # x values
-x_values = [0, 1, 2, 3]
+x = [0, 1, 2, 3, 4]
 
-# x ** 2 values
-x2_values = [0, 1, 4, 9]
+# first function is the same as x
+y1 = x
 
-# 2 ** x values
-x3_values = [1, 2, 4, 8]
+# using a for loop to get each function for below y list values
+# second function is x**2
+y2 = []
+for i in x:
+    i = i ** 2
+    y2.append(i)
 
+# third function is 2**x
+y3 = []
+for i in x:
+    i = 2 ** i
+    y3.append(i)
 
-# no y values given so this uses the range of the length of each variable as the y functions
-plt.plot(x_values, range(len(x_values)))
-plt.plot(x2_values, range(len(x2_values)))
-plt.plot(x3_values, range(len(x3_values)))
+# building plots below
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.plot(x, y3)
+
+# to display the graph
 plt.show()
+# outputs the 3 different plotlines on one graph.
