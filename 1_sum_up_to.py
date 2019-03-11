c436a436762038f5b28f92f600a21918352b27d1
Jennifer Ryan <G00376325@gmit.ie>
2019-03-11 22:26:08 +0000

    Code complete. Minor comment edits - clean up

--- 1_sum_up_to.py
+++ 1_sum_up_to.py
@@ -1,27 +1,24 @@
-# Write a program that asks the user to input any positive integer outputs the sum of all numbers between one and that number.
+# Problem: Write a program that asks the user to input any positive integer and outputs the sum of all numbers between one and that number.
 
-# import getInteger function from pcinput.py to ensure input is an integer. Set value to variable i.
+# Import getInteger function from pcinput.py to ensure input is an integer. Set value to variable i.
 from pcinput import getInteger
 
 i = getInteger("Please enter a positive integer: ")
 
 # getInteger does not check for negative numbers. i needs to be a positive integer only.
-# A while loop that only breaks when a positive int is entered? 
-# got help from https://stackoverflow.com/questions/39855326/how-to-ask-a-string-again-if-the-wrong-answer-is-entered)
-# asks user to input again if number is negative. 
-# Is it bad code to have a second input prompt rather than trying to start the program again?
+# Using a while loop that only breaks when a positive integer is entered.
+# help from https://stackoverflow.com/questions/39855326/how-to-ask-a-string-again-if-the-wrong-answer-is-entered)
 while i < 0:
     print("That is not a positive integer -- please try again.")
     i = getInteger("Please enter a positive integer: ")
 
 
-# Using a for loop
-# need range from 1 to i + 1 in order to include the user input as the range() function takes you up to but does not include the second number.
-# set variable answer to zero then work through each iteration, adding to answer at end of each loop
+# Using a for loop to get the sum of numbers.
+# Need range from 1 to i + 1 in order to include the user input as the range() function takes you up to but does not include the second number.
+# Set variable answer to zero then work through each iteration, adding to answer at end of each loop
 # 0 + 1 - answer becomes 1
 # 1 + 2 - answer becomes 3
-# 3 + 3 - answer becomes 6
-# 6 + 4 - answer becomes 10 .....
+# 3 + 3 - answer becomes 6.....
 
 answer = 0
 for num in range(1, i + 1):
