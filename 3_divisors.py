a3349ecbbf3a801b70766c953a3fa2516e94c2a1
Jennifer Ryan <G00376325@gmit.ie>
2019-03-16 15:29:38 +0000

    Code complete

--- 3_divisors.py
+++ 3_divisors.py
@@ -1,13 +1,11 @@
-# Write a program that prints all numbers between 1,000 and 10,000 that are divisible by 6 but not 12.
+# Problem: Write a program that prints all numbers between 1,000 and 10,000 that are divisible by 6 but not 12.
 
 # range() up to 10001 to include 10000 
 
-# for loop with modulo 
+# for loop with modulo and if statement
 # Two conditions need to be true for each number:
-# 1. The remainder when divided by 6 must be zero
-# 2. The remainder when divided by 12 must not be zero (i.e should be greater than zero) 
-
-
+# 1. When divided by 6 the remainder must be zero
+# 2. When divided by 12 the remainder must not be zero (i.e should be greater than zero) 
 for i in range(1000, 10001):
     if (i % 6 == 0) and (i % 12 > 0):
         print(i)
