4a9ca456c05fab13d6b80924d032536092559f86
Jennifer Ryan <G00376325@gmit.ie>
2019-03-16 16:31:28 +0000

    Code complete

--- 4_collatz.py
+++ 4_collatz.py
@@ -1,22 +1,15 @@
-# Write a program that asks the user to input any positive integer and outputs the successive values of the following calculation. 
+# Problem: 
+# Write a program that asks the user to input any positive integer and outputs the successive values of the following calculation: 
 # At each step calculate the next value by taking the current value and, 
-# if it is even, divide it by two, 
-# but if it is odd, multiply it by three and add one. 
+# if it is even, divide it by two, but if it is odd, multiply it by three and add one. 
 # Have the program end if the current value is one.
 
-# First attempt:
-# Will just using "while i > 1" suffice to stop the code at 1? - was not aware of collatz conjecture until Skype chat 20.02.19. Since the sequence will always end at one, this while function should suffice.
-# if loop for each condition
-# Need to print i to show first number
-# Need to add int before printing i each time to get rid of decimal point
-# Results printing one on top of the other unlike example output - SHOULD I MAKE THIS A LIST? str() does not work.
-# Still need to test for input other than an integer!
-
-# import getInteger to get user input and ensure i is positive.
+# Import getInteger to get user input and set to variable i.
 from pcinput import getInteger
 
 i = getInteger("Please enter a positive integer: ")
 
+# Ensure i is positive.
 while i < 0:
     print("That is not a positive integer -- please try again.")
     i = getInteger("Please enter a positive integer: ")
@@ -24,18 +17,17 @@ while i < 0:
 # i added to list num_list, which will contain the sequence of numbers for the final output.
 num_list = [i]
 
-
+# Collatz conjecture states that any series of numbers created from the 2 calculations will end in 1, no matter the value of i
+# Thus the while loop is set to end when it reaches 1.
 while i > 1:
-    if i % 2 == 0:
-        i = i/2
-        num_list.append(int(i))
-    elif i % 2 == 1:
-        i = (i * 3) + 1
-        num_list.append(int(i))
+    if i % 2 == 0:              # even number
+        i = i/2                 # divide by 2         
+        num_list.append(int(i)) # add to list - int to remove decimal
+    elif i % 2 == 1:            # odd number
+        i = (i * 3) + 1         # multiply by 3 and add 1
+        num_list.append(int(i)) # add to list - int to remove decimal
 
-
-# To allow the numbers to print as shown in the example answer 
-# i.e. in a row without the brackets and commas of a printed list 
-# I used a variation of the following answer from stack overflow: https://stackoverflow.com/a/17757544
-# Only the asterisk operator is needed
+# To allow the numbers to print as shown in the example answer, i.e. in a row without the brackets and commas of a printed list 
+# Used a variation of the following answer from stack overflow: https://stackoverflow.com/a/17757544
+# Only the asterisk operator is needed - removes brackets and commas
 print (*num_list)
