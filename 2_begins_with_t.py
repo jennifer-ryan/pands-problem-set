e8358236d14c091c3261d5894138e73ec11859e3
Jennifer Ryan <G00376325@gmit.ie>
2019-03-16 15:20:56 +0000

    Code complete

--- 2_begins_with_t.py
+++ 2_begins_with_t.py
@@ -1,12 +1,13 @@
 # Write a program that outputs whether or not today is a day that begins with the letter T. 
 
-# indexing?
-# similar to tuesday.py from week 1?
-# Only days that begin with T are Tuesday (index 1) and Thursday (index 3)
-# So if either are True, print the "Yes..." string
+# indexing of days in datetime
+# similar to tuesday.py from week 1
 
-
+# Import datetime module which determines the day of the week
 import datetime
+
+# The only days that begin with T are Tuesday (index 1 in datetime) and Thursday (index 3 in datetime)
+# If either are True, print the "Yes..." string
 
 if (datetime.datetime.today().weekday() == 1) or (datetime.datetime.today().weekday() == 3):
   print("Yes - today begins with a T.")
