aa66dcf168f496e16238a1f428417ecf36af01e8
Jennifer Ryan <G00376325@gmit.ie>
2019-03-16 20:28:17 +0000

    Code complete

--- 6_second_string.py
+++ 6_second_string.py
@@ -1,40 +1,37 @@
-# Write a program that takes a user input string and outputs every second word.
+# Problem: Write a program that takes a user input string and outputs every second word.
 
-# The example starts from first word so I will do the same.
 
-# Possible to index each word by dividing the sentence according to the space between them?
-# Can use the .split() function for this. If no parameter entered, whitespace is used by default
-# .split() creates a list of words that can be iterated through (list_sentence)
-# Use a for loop and modulo to iterate through the list. I want indexes 0, 2, 4, 6, etc. 
-# If the modulo of the index is 0, amend list_sentence to add each word - this returns every second word and leaves out every word with an index that has a modulo >0.
-# This returns a list, but I need a string. Could this be fixed by simply adding str() when printing? <- Doesn't work - still prints a list
-# Use the .join() method instead - this is the opposite of .split(). Separate each item on the list by a space(" ") to create a string (string_sentence).
 # To account for punctuation, try to create a for loop that goes through each character in string_sentence and checks for punctuation with the string.punctuation constant (import string first)
 # Rather than removing punctuation, it might be better to check for characters not in string.punctuation and creating a final sentence <- it works!
 
-import string
+import string # To use string.punctuation at the end.
 
-sentence = input("Please enter a sentence: ")
+# Import getString from pcinput.py.
+from pcinput import getString
 
+# Prompt user to enter a string and save under the variable sentence. 
+sentence = getString("Please enter a sentence: ")
+
+# Breaks sentence into a list of words that can be iterated through.
 words = sentence.split()
 
-list_sentence = []
-
-index = 0
-
+list_sentence = [] # List will save every second word in words
+index = 0 # mimics iterating through the index of words
 for w in words:
-    if index % 2 == 0:
-        list_sentence.append(w)
-    index += 1
+    if index % 2 == 0: # returns indexes 0, 2, 4, 6, etc.
+        list_sentence.append(w) # adds word to new list
+    index += 1 # moves on to the next index
 
-
+# Creates a string from list_sentence separated by a space
 string_sentence = " ".join(list_sentence)
 
+# Variable for final output
 final_sentence = ""
 
+# Iterates through each character to get rid of punctuation 
 for x in string_sentence:
     if x not in string.punctuation:
-        final_sentence += x
+        final_sentence += x 
 
-
+# print to console
 print(final_sentence)
