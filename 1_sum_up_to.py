e6b4d3b86f69cbb96c97bc5f41d13b15d795fd71
Jennifer Ryan <G00376325@gmit.ie>
2019-03-16 15:26:11 +0000

    Code complete

--- 1_sum_up_to.py
+++ 1_sum_up_to.py
@@ -7,11 +7,10 @@ i = getInteger("Please enter a positive integer: ")
 
 # getInteger does not check for negative numbers. i needs to be a positive integer only.
 # Using a while loop that only breaks when a positive integer is entered.
-# help from https://stackoverflow.com/questions/39855326/how-to-ask-a-string-again-if-the-wrong-answer-is-entered)
+# Help from https://stackoverflow.com/a/39855435
 while i < 0:
     print("That is not a positive integer -- please try again.")
     i = getInteger("Please enter a positive integer: ")
-
 
 # Using a for loop to get the sum of numbers.
 # Need range from 1 to i + 1 in order to include the user input as the range() function takes you up to but does not include the second number.
@@ -19,11 +18,9 @@ while i < 0:
 # 0 + 1 - answer becomes 1
 # 1 + 2 - answer becomes 3
 # 3 + 3 - answer becomes 6.....
-
 answer = 0
 for num in range(1, i + 1):
     answer += num
     
 # print answer to console.
-print(answer)
-    
+print(answer) 
