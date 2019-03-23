302cb07a6dd2585bc051457ef4c3d11239f79c02
Jennifer Ryan <G00376325@gmit.ie>
2019-03-23 11:51:26 +0000

    added suffix function. Should I try to get am/pm as lower case?

--- new_datetime.py
+++ new_datetime.py
@@ -5,6 +5,25 @@
 
 import datetime as dt 
 
-# Using # instead of - for windows formatting https://stackoverflow.com/a/2073189
-print(dt.datetime.strftime(dt.datetime.now(), "%A, %B %#d %Y at %#I:%M%p"))
+# function created from original datetime solution
+def suffix():
+    date = dt.datetime.today().day
+    suf = ""
 
+    if date == 1 or date == 21 or date == 31:
+        suf = "st"  
+    elif date == 2 or date == 22:
+        suf = "nd"
+    elif date == 3 or date == 23:
+        suf = "rd"
+    else:
+        suf = "th" 
+    return suf     
+
+
+# Using # instead of - for windows formatting https://stackoverflow.com/a/2073189
+# broken into 2 variables to add correct suffix in the middle
+first = dt.datetime.strftime(dt.datetime.now(), "%A, %B %#d") 
+second = dt.datetime.strftime(dt.datetime.now(),"%Y at %#I:%M%p")
+
+print (first+suffix(), second)
