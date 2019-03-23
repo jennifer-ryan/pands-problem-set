d5e0dd8d3eff6d07697fb7c4c578f914ebbb35b8
Jennifer Ryan <G00376325@gmit.ie>
2019-03-23 12:22:29 +0000

    Replaced with new code that was originally in new_datetime.py as old code very long and inefficient.

--- 8_datetime.py
+++ 8_datetime.py
@@ -1,99 +1,39 @@
-# Write a program that outputs today’s date and time in the format 
-# "Monday, January 10th 2019 at 1:15pm".
+# Problem: Write a program that outputs today’s date and time in the format "Monday, January 10th 2019 at 1:15pm".
+# Note: This is an entirely different version of original code using strftime. 
+# Please see old commits for original solution, which worked but was very inefficient.
 
 # Import datetime to get access to date and time
-from datetime import datetime
+import datetime as dt 
 
-# Day. On datetime 0 = Monday, 1 = Tuesday, 2 = Wednesday, etc. 
-# Converted each number to a string in variable d.
-day = datetime.today().weekday()
-d = ""
+# Broken into 2 variables to add correct suffix for the date in the middle
+# first returns day, month, date
+first = dt.datetime.strftime(dt.datetime.now(), "%A, %B %#d") # Windows formatting uses '#' instead of '-' https://stackoverflow.com/a/2073189
+# second returns year and time
+second = dt.datetime.strftime(dt.datetime.now(),"%Y at %#I:%M")
 
-if day == 0:
-    d = "Monday"
-elif day == 1:
-    d = "Tuesday"
-elif day == 2:
-    d = "Wednesday"
-elif day == 3:
-    d = "Thursday"
-elif day == 4:
-    d = "Friday"
-elif day == 5:
-    d = "Saturday"
-elif day == 6:
-    d = "Sunday"                    
+# Function suffix() created from original datetime solution to get suffixes for the date ('st', 'nd', 'rd' or 'th')
+def suffix():
+    date = dt.datetime.today().day
+    suf = ""
+    if date == 1 or date == 21 or date == 31:
+        suf = "st"  
+    elif date == 2 or date == 22:
+        suf = "nd"
+    elif date == 3 or date == 23:
+        suf = "rd"
+    else:
+        suf = "th" 
+    return suf     
 
+# Cannot get %p in strftime to print am/pm as lowercase on Windows so wrote function below for this, based on what was written in first solution
+def ampm():
+    hour = dt.datetime.today().hour
+    end = ""
+    if hour in range(0, 12):
+        end = "am"
+    else:
+        end = "pm"
+    return end    
 
-# Month. Same technique as above. 
-month = datetime.today().month  
-m = ""
-
-if month == 1:
-    m = "January"
-elif month == 2:
-    m = "February"
-elif month == 3:
-    m = "March"    
-elif month == 4:
-    m = "April"
-elif month == 5:
-    m = "May"
-elif month == 6:
-    m = "June"
-elif month == 7:
-    m = "July"
-elif month == 8:
-    m = "August"
-elif month == 9:
-    m = "September"
-elif month == 10:
-    m = "October"
-elif month == 11:
-    m = "November"
-elif month == 12:
-    m = "December"
-
-# Date
-# Each date concatenated with appropriate suffixes 
-date = datetime.today().day
-dte = ""
-
-if date == 1 or date == 21 or date == 31:
-    dte = str(date) + "st"  
-elif date == 2 or date == 22:
-    dte = str(date) + "nd"
-elif date == 3 or date == 23:
-    dte = str(date) + "rd"
-else:
-    dte = str(date) + "th"             
-
-# Year
-year = datetime.today().year
-
-# Hour 
-hour = datetime.today().hour
-hr = ""
-
-if hour in range(13, 24):
-    hr = hour - 12 # accounting for 12 hour clock
-elif hour == 0: 
-    hr = 12 # accounting for 0 hour
-else: 
-    hr = hour
-
-# Minute
-minute = datetime.today().minute
-min = str(minute)
-# single digit minutes need a 0 prefix
-if minute in range(0, 10):
-    min = "0" + min     
-
-# Adds am or pm to the end of the string.
-if hour in range(0, 12):
-    min += "am"
-else:
-    min += "pm"
-
-# print complete string to console.
-print(d + ", " + m, dte, year, "at", str(hr) + ":" + min)
+# Concatenate within print function to print whole string to console
+print (first+suffix(), second+ampm())
