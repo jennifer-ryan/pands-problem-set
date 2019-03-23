89899f2356a3259812aa25303e55a8c1d3550928
Jennifer Ryan <G00376325@gmit.ie>
2019-03-23 14:25:39 +0000

    adjusted pcinput.py to accept positive integers and floats only

--- pcinput.py
+++ pcinput.py
@@ -1,11 +1,16 @@
 # pcinput.py is a module created by Pieter Spronck for his book 'The Coder's Apprentice'. 
 # Below I have taken three functions from this module which will be used in several of the problems.
+# All problems asking the user to input an integer or float require a positive number 
+# Thus, I have adjusted the functions getFloat and getInteger so that negative numbers are not accepted as input.  
 
-
-def getFloat( prompt ):
+def getFloat(prompt):
     while True:
         try:
             num = float( input( prompt ) )
+            # added if statement below if negative number entered
+            if num < 0:
+                print ("That is not a positive number - please try again")
+                continue
         except ValueError:
             print( "That is not a number -- please try again" )
             continue
@@ -15,6 +20,10 @@ def getInteger( prompt ):
     while True:
         try:
             num = int( input( prompt ) )
+            # added if statement below if negative number entered
+            if num < 0:
+                print ("That is not a positive number - please try again")
+                continue
         except ValueError:
             print( "That is not an integer -- please try again" )
             continue
