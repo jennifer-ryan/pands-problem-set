467d560f4b7083219f4329f52ee96e32b23a0df7
Jennifer Ryan <G00376325@gmit.ie>
2019-03-23 17:57:25 +0000

    Added with to open file as suggested in lecture. Removed f.close()

--- 9_second_line.py
+++ 9_second_line.py
@@ -11,23 +11,19 @@
 # Will only work if file is in same folder.          
 txt_file = input("Please enter a text file name: ")
 
-# Open the file
-f = open(txt_file)
+# Open the file. Lecture https://web.microsoftstream.com/video/72484dfc-1b50-4223-8039-bd6a69cab573
+with open(txt_file) as f: 
+    # Using .readlines() method to access individual lines https://stackoverflow.com/a/42040147
+    # It creates a list with each line as a separate item on the list
+    lines = f.readlines()
 
-# Using .readlines() method to access individual lines https://stackoverflow.com/a/42040147
-# It creates a list with each line as a separate item on the list
-lines = f.readlines()
+    # If variable lines is printed at this stage, it comes up as a list with blank lines shown as '\n'
+    # below removes the blanks
+    for l in lines:
+        if l == '\n':
+            lines.remove(l)
 
-# If variable lines is printed at this stage, it comes up as a list with blank lines shown as '\n'
-# below removes the blanks
-for l in lines:
-    if l == '\n':
-        lines.remove(l)
-
-# for loop - range starts at 0, ends with the number of lines in the text and does it in increments of 2.
-# i represets the index of each line in lines and every second one should print to console
-for i in range(0, len(lines), 2):
-    print (lines[i])
-
-#close the  file
-f.close()
+    # for loop - range starts at 0, ends with the number of lines in the text and does it in increments of 2.
+    # i represets the index of each line in lines and every second one should print to console
+    for i in range(0, len(lines), 2):
+        print (lines[i])
