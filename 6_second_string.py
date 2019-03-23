da3b31361cf9614bdcb49fa189a399463f1d4ef8
Jennifer Ryan <G00376325@gmit.ie>
2019-03-23 15:49:46 +0000

    changed punctuation remover as I want to keep apostrophes

--- 6_second_string.py
+++ 6_second_string.py
@@ -1,10 +1,6 @@
 # Problem: Write a program that takes a user input string and outputs every second word.
 
-
-# To account for punctuation, try to create a for loop that goes through each character in string_sentence and checks for punctuation with the string.punctuation constant (import string first)
-# Rather than removing punctuation, it might be better to check for characters not in string.punctuation and creating a final sentence <- it works!
-
-import string # To use string.punctuation at the end.
+import re # will be used to remove punctuation for final output
 
 # Import getString from pcinput.py.
 from pcinput import getString
@@ -15,23 +11,23 @@ sentence = getString("Please enter a sentence: ")
 # Breaks sentence into a list of words that can be iterated through.
 words = sentence.split()
 
-list_sentence = [] # List will save every second word in words
+list_second = [] # this list will save every second word in words
 index = 0 # mimics iterating through the index of words
 for w in words:
     if index % 2 == 0: # returns indexes 0, 2, 4, 6, etc.
-        list_sentence.append(w) # adds word to new list
+        list_second.append(w) # adds every second word to new list
     index += 1 # moves on to the next index
 
-# Creates a string from list_sentence separated by a space
-string_sentence = " ".join(list_sentence)
+# Create a string from list_second separated by a space
+string_sentence = " ".join(list_second)
 
-# Variable for final output
-final_sentence = ""
-
-# Iterates through each character to get rid of punctuation 
-for x in string_sentence:
-    if x not in string.punctuation:
-        final_sentence += x 
+# Output will print words including punctuation as part of the word 
+# Should be no punctuation except for apostrophes that are part of some words (they're, we're, etc.)
+# Initially tried string.punctuation but could not exclude apostrophes from being removed
+# Search and replace. re.sub help from https://stackoverflow.com/a/47561619
+# The character ^ indicates that what follows should not be replaced, i.e.uppercase and lowercase letters, apostrophes and spaces. 
+# All other punctuation is replaced with nothing ''
+final_sentence = re.sub("[^a-zA-Z' ]", '', string_sentence) 
 
 # print to console
 print(final_sentence)
