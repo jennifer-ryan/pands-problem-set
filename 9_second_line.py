bdb0671477bc0c7d143976dd894a58c214f21102
Jennifer Ryan <G00376325@gmit.ie>
2019-03-23 14:58:05 +0000

    tidying up comments. No adjustments to code

--- 9_second_line.py
+++ 9_second_line.py
@@ -1,6 +1,4 @@
-# Write a program that reads in a text file and outputs every second line. 
-# The program should take the filename from an argument on the command line.
-
+# Problem: Write a program that reads in a text file and outputs every second line. The program should take the filename from an argument on the command line.
 
 # Help from this video https://www.youtube.com/watch?v=qjdeQ83T9sQ 
 # and this stack overflow question https://stackoverflow.com/questions/32470543/open-file-in-another-directory-python
@@ -8,34 +6,27 @@
 
 # daffodils.txt in folder for testing purposes
 
-# user will need to input path to file if it is not located in same folder they are running the code from. 
-
 # Ask user to enter file name.
 # Will only work if file is in same folder.          
 txt_file = input("Please enter a text file name: ")
 
-# open the file
+# Open the file
 f = open(txt_file)
 
-# using .readlines() method to access lines https://stackoverflow.com/a/42040147
-# looks like it creates a list with each line as a separate item on the list
-# indexing to get every second line? 
+# Using .readlines() method to access individual lines https://stackoverflow.com/a/42040147
+# It creates a list with each line as a separate item on the list
 lines = f.readlines()
 
-# if I print lines at this stage, it comes up as a list with blank lines shown as '\n'
-# maybe I can just remove these from the list?
-# it works!
+# If variable lines is printed at this stage, it comes up as a list with blank lines shown as '\n'
+# below removes the blanks
 for l in lines:
     if l == '\n':
         lines.remove(l)
 
 # for loop - range starts at 0, ends with the number of lines in the text and does it in increments of 2.
-# Does not start at first line though - does if I start the range at 0
-# i is the index of each line in lines and every second one should print to console
-# prints every second line but does not start at first line and also counts blank lines - problem solved
+# i represets the index of each line in lines and every second one should print to console
 for i in range(0, len(lines), 2):
     print (lines[i])
 
 #close the  file
 f.close()
- 
