c4383020256ac5e4c3fe1aa1322a9bcbc4d2344d
Jennifer Ryan <G00376325@gmit.ie>
2019-03-31 15:16:51 +0100

    Complete

--- 6_second_string.py
+++ 6_second_string.py
@@ -1,8 +1,9 @@
 # Problem: Write a program that takes a user input string and outputs every second word.
 
-import re # will be used to remove punctuation for final output
+# Import re to remove punctuation for final output.
+import re 
 
-# Import getString from pcinput.py.
+# Import getString from pcinput.py to remove leading/trailing spaces.
 from pcinput import getString
 
 # Prompt user to enter a string and save under the variable sentence. 
@@ -15,14 +16,14 @@ list_second = [] # this list will save every second word in words
 index = 0 # mimics iterating through the index of words
 for w in words:
     if index % 2 == 0: # returns indexes 0, 2, 4, 6, etc.
-        list_second.append(w) # adds every second word to new list
+        list_second.append(w) # adds every second word to the new list
     index += 1 # moves on to the next index
 
 # Create a string from list_second separated by a space
 string_sentence = " ".join(list_second)
 
 # Output will print words including punctuation as part of the word 
-# Should be no punctuation except for apostrophes that are part of some words (they're, we're, etc.)
+# There should be no punctuation except for apostrophes that are part of some words (they're, we're, etc.)
 # Initially tried string.punctuation but could not exclude apostrophes from being removed
 # Search and replace. re.sub help from https://stackoverflow.com/a/47561619
 # The character ^ indicates that what follows should not be replaced, i.e.uppercase and lowercase letters, apostrophes and spaces. 
