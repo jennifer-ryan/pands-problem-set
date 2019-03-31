a40a6e1f0b3f2babb3d764e84c090a7acb1c5c55
Jennifer Ryan <G00376325@gmit.ie>
2019-03-31 15:24:53 +0100

    Complete

--- 7_square_root.py
+++ 7_square_root.py
@@ -1,17 +1,18 @@
 # Problem: Write a program that that takes a positive floating point number as input and outputs an approximation of its square root.
 
-# import square root function from math module
+# Import square root function from math module.
 from math import sqrt
 
-# Import getFloat to get user input and set to variable i.
+# Import getFloat function from pcinput.py to ensure input is a positive floating point number. 
 from pcinput import getFloat
 
+# Prompt user to enter a positive floating point number and set value to variable i.
 i = getFloat("Please enter a positive number: ")
 
-# Formats s so there is only 1 decimal place.
+# Format s so there is only 1 decimal place.
 s = round(sqrt(i), 1)
 
-# if s is the same as int(s), the number is a whole number square root and not approximate so the output is adjusted.
+# If s is the same as int(s), the number is a whole number square root and not approximate so the output is adjusted.
 # Help from https://stackoverflow.com/q/4541155
 if s != int(s):
     print("The square root of", i, "is approximately " + str(s) + ".")
