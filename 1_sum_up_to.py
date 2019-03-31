d347b901ce8379ac3887b83a9a14beb6124bfe78
Jennifer Ryan <G00376325@gmit.ie>
2019-03-31 14:22:20 +0100

    Complete

--- 1_sum_up_to.py
+++ 1_sum_up_to.py
@@ -1,16 +1,14 @@
 # Problem: Write a program that asks the user to input any positive integer and outputs the sum of all numbers between one and that number.
 
-# Import getInteger function from pcinput.py to ensure input is a positive integer. Set value to variable i.
+# Import getInteger function from pcinput.py to ensure input is a positive integer. 
 from pcinput import getInteger
 
+# Prompt user to enter a positive integer and set value to variable i.
 i = getInteger("Please enter a positive integer: ")
 
 # Using a for loop to get the sum of numbers.
-# Need range from 1 to i + 1 in order to include the user input as the range() function takes you up to but does not include the second number.
+# Need range from 1 to i + 1 in order to include the user input.
 # Set variable answer to zero then work through each iteration, adding to answer at end of each loop
-# 0 + 1 - answer becomes 1
-# 1 + 2 - answer becomes 3
-# 3 + 3 - answer becomes 6.....
 answer = 0
 for num in range(1, i + 1):
     answer += num
